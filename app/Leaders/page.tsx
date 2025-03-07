01dee921e43ba1cf192d5796b60a49adfe2c5aef
TheKany <skarnjs06@naver.com>
2025-03-07 09:50:39 +0900

    스크롤 방식 업데이트

--- app/Leaders/page.tsx
+++ app/Leaders/page.tsx
@@ -1,19 +1,9 @@
 "use client";
 
 import Image from "next/image";
-import React, { useState } from "react";
-import Slider from "react-slick";
 import styled from "styled-components";
 
 const LeaderPage = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    slidesToScroll: 1,
-    speed: 500,
-    cssEase: "linear",
-  };
-
   const data = [
     {
       season: 1,
@@ -43,39 +33,31 @@ const LeaderPage = () => {
   ];
 
   return (
-    <>
+    <Container>
       <Title>식서스 역대 팀장</Title>
-      <Slider {...settings}>
-        {data.map((el) => (
-          <Slide key={el.season}>
-            <SeasonTitle>시즌 {el.season}</SeasonTitle>
-            <ImageWithSkeleton season={`${el.season}`} />
-          </Slide>
-        ))}
-      </Slider>
-    </>
-  );
-};
-
-const ImageWithSkeleton = ({ season }: { season: string }) => {
-  const [imageLoaded, setImageLoaded] = useState(false);
-
-  return (
-    <ImageWrapper>
-      {!imageLoaded && <Skeleton />}
-      <StyledImage
-        src={`/img/s${season}/leader.png`}
-        alt={`시즌${season} 팀장`}
-        width={300}
-        height={220}
-        onLoad={() => setImageLoaded(true)}
-        $loaded={imageLoaded}
-      />
-    </ImageWrapper>
+      {data.map((el) => (
+        <div key={el.season}>
+          <SeasonTitle>시즌 {el.season}</SeasonTitle>
+          <Image
+            src={`/img/s${el.season}/leader.png`}
+            alt={`시즌${el.season} 팀장`}
+            width={300}
+            height={220}
+          />
+        </div>
+      ))}
+    </Container>
   );
 };
 
 export default LeaderPage;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 const Title = styled.p`
   font-size: 25px;
@@ -88,42 +70,6 @@ const SeasonTitle = styled.p`
   font-size: 18px;
   font-weight: 500;
   font-family: "Jua";
-`;
-
-const Slide = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-const ImageWrapper = styled.div`
-  position: relative;
-  width: 300px;
-  height: 220px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const Skeleton = styled.div`
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  background: linear-gradient(90deg, #e0e0e0 25%, #f8f8f8 50%, #e0e0e0 75%);
-  background-size: 200% 100%;
-  animation: skeleton-loading 1.5s infinite;
-
-  @keyframes skeleton-loading {
-    0% {
-      background-position: 200% 0;
-    }
-    100% {
-      background-position: -200% 0;
-    }
-  }
-`;
-
-const StyledImage = styled(Image)<{ $loaded: boolean }>`
-  display: ${({ $loaded }) => ($loaded ? "block" : "none")};
-  transition: opacity 0.3s ease-in-out;
+  text-align: center;
+  margin-top: 16px;
 `;
